2e070f86f6efbb0392f4336bb52b642bcaa512e5
Paul Herron <contact@paulherron.net>
2017-01-07 22:03:00 +0000

    Use consistent naming to denote which command was run last.

--- www/index.php
+++ www/index.php
@@ -24,7 +24,7 @@ exec($command, $output, $exitCode);
 
 $response = array(
 	'error' => (bool) $exitCode,
-	'action' => $action,
+	'lastCommand' => $action,
 	'switch' => $switch,
 	'output' => $output
 );
